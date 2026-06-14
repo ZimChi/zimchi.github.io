0292feb9bee8b7d09accd40daeacd890dfb21fd7
zimchi <chielo.zimmerman@gmail.com>
2026-06-14 16:34:06 -0400

    cleans up some development artifacts

--- js/article.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.tldr').forEach(tldr => {
-
-  tldr.addEventListener('click', () => {
-    console.log("click")
-    const target = document.getElementById(tldr.dataset.target);
-    target.style.display = target.style.display === 'block' ? 'none' : 'block';
-  });
-});
